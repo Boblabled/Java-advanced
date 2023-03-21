310a738b7b68babc943eca0d7a2ed1efd16f776d
Pavel Sharaev <mail@pechhenka.ru>
2023-03-21 20:02:59 +0300

    checked

--- Ввод-вывод/Walk.java
+++ Ввод-вывод/Walk.java
@@ -10,56 +10,55 @@ import java.nio.file.Path;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
-import static java.nio.file.Files.*;
 
 public class Walk {
 
-    private static final String ERROR_HASH = "0".repeat(64);
-    private static byte[] buffer;
+    private final byte[] errorHash;
+    private final byte[] buffer;
+    private final MessageDigest digest;
 
     public Walk(int buffer_size) {
         buffer = new byte[buffer_size];
+        try {
+            this.digest = MessageDigest.getInstance("SHA-256");
+        } catch (NoSuchAlgorithmException e) {
+            throw new WalkException("Not found algorithm", e);
+        }
+        errorHash = new byte[digest.getDigestLength()];
     }
 
     public static void main(String[] args) {
-        Walk walk = new Walk(4096);
-        walk.run(args);
+        try {
+            Walk walk = new Walk(4096);
+            walk.run(args);
+        } catch (WalkException e) {
+            System.err.println(e.getMessage());
+        }
     }
 
-    private void run(String[] args){
+    private void run(String[] args) {
         if (args == null || args.length != 2 || args[0] == null || args[1] == null) {
-            return;
+            throw new WalkException("Bad args", null);
         }
 
-        Path inputPath;
-        try {
-            inputPath = Path.of(args[0]);
-        } catch (InvalidPathException e) {
-            System.err.println("Fail open output file: " + e.getMessage());
-            return;
-        }
-
-        Path outputPath;
-        try {
-            outputPath = Path.of(args[1]);
-        } catch (InvalidPathException e) {
-            System.err.println("Fail open output file: " + e.getMessage());
-            return;
-        }
+        Path inputPath = getPath(args[0], "input");
+        Path outputPath = getPath(args[1], "output");
 
         try {
             if (outputPath.getParent() != null) {
                 Files.createDirectories(outputPath.getParent());
             }
         } catch (IOException e) {
-            System.err.println("Fail make directories: " + e.getMessage());
+            throw new WalkException("Fail make dirs", e);
         }
 
-        try (BufferedReader bufferedReader = newBufferedReader(inputPath, StandardCharsets.UTF_8)) {
-            try (BufferedWriter bufferedWriter = newBufferedWriter(outputPath, StandardCharsets.UTF_8)) {
+        try (BufferedReader bufferedReader = Files.newBufferedReader(inputPath, StandardCharsets.UTF_8)) {
+            try (BufferedWriter bufferedWriter = Files.newBufferedWriter(outputPath, StandardCharsets.UTF_8)) {
                 String file_path;
+                // :NOTE: bufferedReader.readLine() error message
                 while ((file_path = bufferedReader.readLine()) != null) {
-                    bufferedWriter.write(hashigFile(file_path) + " " + file_path);
+                    byte[] hash = hashigFile(file_path);
+                    bufferedWriter.write(convertHash(hash) + " " + file_path);
                     bufferedWriter.newLine();
                 }
             } catch (IOException e) {
@@ -70,39 +69,50 @@ public class Walk {
         }
     }
 
-    private static String hashigFile(String fileName) {
-        MessageDigest digest = null;
+    private static Path getPath(String path, String name) {
         try {
-            digest = MessageDigest.getInstance("SHA-256");
-        } catch (NoSuchAlgorithmException e) {
-            System.err.println("Wrong hash algorithm: " + e.getMessage());
-        }
-        try {
-            Path path = Path.of(fileName);
+            return Path.of(path);
         } catch (InvalidPathException e) {
+            throw new WalkException("Fail convert " + name + " path", e);
+        }
+    }
+
+    private byte[] hashigFile(String fileName) {
+        Path path;
+        try {
+            path = getPath(fileName, "");
+        } catch (Exception e) {
             System.err.println("Fail open file: " + e.getMessage());
-            return ERROR_HASH;
+            return errorHash;
         }
 
-        try (InputStream fis = newInputStream(Path.of(fileName))){
+        digest.reset();
+
+        try (InputStream fis = Files.newInputStream(path)) {
             int count;
-            while((count=fis.read(buffer))!=-1) {
-                for (int i = 0; i < count; i++) {
-                    assert digest != null;
-                    digest.update(buffer[i]);
-                }
+            while ((count = fis.read(buffer)) != -1) {
+                digest.update(buffer, 0, count);
             }
         } catch (IOException e) {
             System.err.println("Fail read file: " + e.getMessage());
-            return ERROR_HASH;
+            return errorHash;
         }
-        assert digest != null;
-        byte[] hash = digest.digest();
-        BigInteger sum =  new BigInteger(1, hash);
+
+        return digest.digest();
+    }
+
+    private String convertHash(byte[] hash) {
+        BigInteger sum = new BigInteger(1, hash);
         StringBuilder hexString = new StringBuilder(sum.toString(16));
         while (hexString.length() < 64) {
             hexString.insert(0, '0');
         }
         return hexString.toString();
+    }
+
+    private static class WalkException extends RuntimeException {
+        public WalkException(String message, Throwable e) {
+            super(message, e);
+        }
     }
 }
