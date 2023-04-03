3150a3ef744b453105b043b39dc5aa74dfb3d8c5
Pavel Sharaev <mail@pechhenka.ru>
2023-04-03 22:15:19 +0300

    checked

--- Collections Framework/ArraySet.java
+++ Collections Framework/ArraySet.java
@@ -1,34 +1,40 @@
 import java.util.*;
 
-public class ArraySet<E> implements SortedSet<E> {
-    private final int capacity;
-    private final ArrayList<E> elementData;
+// :NOTE: AbstractSet
+public class ArraySet<E> extends AbstractSet<E> implements SortedSet<E> {
+    private final List<E> elementData;
     private final Comparator<? super E> comparator;
 
     public ArraySet() {
         this(Collections.EMPTY_LIST);
     }
 
-    public ArraySet(Collection<? extends E> c, Comparator<? super E> co) {
-        TreeSet<E> set = new TreeSet<>(co);
-        set.addAll(c);
-        this.elementData = new ArrayList<>(set);
-        this.capacity = set.size();
-        this.comparator = co;
-    }
-
+    // :NOTE: duplicate code
     public ArraySet(Collection<? extends E> c) {
         TreeSet<E> set = new TreeSet<>(c);
         this.elementData = new ArrayList<>(set);
-        this.capacity = set.size();
         this.comparator = null;
     }
 
-    public ArraySet(SortedSet<E> c) {
+    public ArraySet(Collection<? extends E> c, Comparator<? super E> co) {
+        this(toList(c, co), co);
+    }
+
+    public ArraySet(Comparator<E> c) {
         TreeSet<E> set = new TreeSet<>(c);
         this.elementData = new ArrayList<>(set);
-        this.capacity = set.size();
         this.comparator = set.comparator();
+    }
+
+    private ArraySet(List<E> data, Comparator<? super E> c) {
+        this.elementData = data;
+        this.comparator = c;
+    }
+
+    private static <E> ArrayList<E> toList(Collection<? extends E> c, Comparator<? super E> co) {
+        TreeSet<E> set = new TreeSet<>(co);
+        set.addAll(c);
+        return new ArrayList<>(set);
     }
 
     @Override
@@ -36,6 +42,7 @@ public class ArraySet<E> implements SortedSet<E> {
         return comparator;
     }
 
+    // :NOTE: duplicate code
     @Override
     public SortedSet<E> subSet(E fromElement, E toElement) {
         if (comparator == null) {
@@ -44,15 +51,18 @@ public class ArraySet<E> implements SortedSet<E> {
         if (comparator.compare(fromElement, toElement) > 0) {
             throw new IllegalArgumentException();
         }
+
+        // 1. Коллекция уже отсортирована
+        // 2. Коллекция не изменяемая
         TreeSet<E> subset = new TreeSet<>(comparator);
-        int from = capacity;
-        for(int i = 0; i < capacity; i++) {
+        int from = elementData.size();
+        for (int i = 0; i < elementData.size(); i++) {
             if (comparator.compare(fromElement, elementData.get(i)) <= 0) {
                 from = i;
                 break;
             }
         }
-        for(int i = from; i < capacity; i++) {
+        for (int i = from; i < elementData.size(); i++) {
             if (comparator.compare(toElement, elementData.get(i)) <= 0) {
                 break;
             }
@@ -67,7 +77,7 @@ public class ArraySet<E> implements SortedSet<E> {
             throw new UnsupportedOperationException("comparator is null");
         }
         TreeSet<E> subset = new TreeSet<>(comparator);
-        for(int i = 0; i < capacity; i++) {
+        for (int i = 0; i < elementData.size(); i++) {
             if (comparator.compare(toElement, elementData.get(i)) <= 0) {
                 break;
             }
@@ -81,8 +91,10 @@ public class ArraySet<E> implements SortedSet<E> {
         if (comparator == null) {
             throw new UnsupportedOperationException("comparator is null");
         }
+//     :NOTE:   Collections.binarySearch()
+//     :NOTE:   List.subList()
         TreeSet<E> subset = new TreeSet<>(comparator);
-        for(int i = capacity-1; i >= 0; i--) {
+        for (int i = elementData.size() - 1; i >= 0; i--) {
             if (comparator.compare(fromElement, elementData.get(i)) > 0) {
                 break;
             }
@@ -93,7 +105,7 @@ public class ArraySet<E> implements SortedSet<E> {
 
     @Override
     public E first() {
-        if (capacity == 0) {
+        if (isEmpty()) {
             throw new NoSuchElementException();
         }
         return elementData.get(0);
@@ -101,84 +113,30 @@ public class ArraySet<E> implements SortedSet<E> {
 
     @Override
     public E last() {
-        if (capacity == 0) {
+        if (isEmpty()) {
             throw new NoSuchElementException();
         }
-        return elementData.get(capacity-1);
+        return elementData.get(elementData.size() - 1);
     }
 
     @Override
     public int size() {
-        return capacity;
+        return elementData.size();
     }
 
     @Override
     public boolean isEmpty() {
-        return capacity == 0;
+        return elementData.size() == 0;
     }
 
     @Override
+    @SuppressWarnings("unchecked")
     public boolean contains(Object o) {
-        return Collections.binarySearch(elementData,(E) o, comparator) >= 0;
+        return Collections.binarySearch(elementData, (E) o, comparator) >= 0;
     }
 
     @Override
     public Iterator<E> iterator() {
         return elementData.iterator();
-    }
-
-    @Override
-    public Object[] toArray() {
-        return elementData.toArray();
-    }
-
-    @Override
-    public <T> T[] toArray(T[] a) {
-        return elementData.toArray(a);
-    }
-
-    @Override
-    public boolean add(E e) {
-        throw new UnsupportedOperationException("add");
-    }
-
-    @Override
-    public boolean remove(Object o) {
-        throw new UnsupportedOperationException("remove");
-    }
-
-    @Override
-    public boolean containsAll(Collection<?> c) {
-        if (comparator == null) {
-            throw new UnsupportedOperationException("comparator is null");
-        }
-        TreeSet<E> set = new TreeSet<>(comparator);
-        set.addAll(elementData);
-        return set.containsAll(c);
-    }
-
-    @Override
-    public boolean addAll(Collection<? extends E> c) {
-        throw new UnsupportedOperationException("addAll");
-    }
-
-    @Override
-    public boolean retainAll(Collection<?> c) {
-        throw new UnsupportedOperationException("retainAll");
-    }
-
-    @Override
-    public boolean removeAll(Collection<?> c) {
-        throw new UnsupportedOperationException("removeAll");
-    }
-
-    @Override
-    public void clear() {
-        throw new UnsupportedOperationException("clear");
-    }
-
-    @Override
-    public String toString() {
-        return elementData.toString();
     }
 }
