61c92c40307af7db344810c96eb02d82f6db6861
Max <max2002abr@mail.ru>
2023-04-07 11:50:12 +0300

    Update StudentDB.java

--- Лямбда-выражения и потоки/StudentDB.java
+++ Лямбда-выражения и потоки/StudentDB.java
@@ -1,40 +1,34 @@
 import java.util.*;
 import java.util.function.BinaryOperator;
 import java.util.function.Function;
-import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class StudentDB implements StudentQuery {
-    // :NOTE: constant
     private static final Comparator<Student> NAME_COMPARATOR =
             Comparator.comparing(Student::getLastName)
                     .thenComparing(Student::getFirstName).reversed()
                     .thenComparing(Student::getId);
 
+    private static final Comparator<Student> DEFAULT_COMPARATOR = Comparator.naturalOrder();
+
     @Override
     public List<String> getFirstNames(List<Student> students) {
-        return getCollect(students, Student::getFirstName);
+        return get(students, Student::getFirstName);
     }
-
-    // :NOTE: dupcode getLastNames, getGroups, getFullNames
 
     @Override
     public List<String> getLastNames(List<Student> students) {
-        return streamToList(getStream(students, Student::getLastName));
+        return get(students, Student::getLastName);
     }
     @Override
     public List<GroupName> getGroups(List<Student> students) {
-        return streamToList(getStream(students, Student::getGroup));
+        return get(students, Student::getGroup);
     }
 
     @Override
     public List<String> getFullNames(List<Student> students) {
-        return streamToList(getStream(students, s -> s.getFirstName() + " " + s.getLastName()));
-    }
-
-    private static ArrayList<String> getCollect(List<Student> students, Function<Student, String> func) {
-        return students.stream().map(func).collect(Collectors.toCollection(ArrayList::new));
+        return get(students, s -> s.getFirstName() + " " + s.getLastName());
     }
 
     @Override
@@ -46,35 +40,33 @@ public class StudentDB implements StudentQuery {
     @Override
     public String getMaxStudentFirstName(List<Student> students) {
         return students.stream()
-                .max(Comparator.naturalOrder())
+                .max(DEFAULT_COMPARATOR)
                 .map(Student::getFirstName).orElse("");
     }
 
-    // :NOTE: избавиться от кучи методов
     @Override
     public List<Student> sortStudentsById(Collection<Student> students) {
-        return streamToList(students.stream().sorted());
+        return sort(students.stream(), DEFAULT_COMPARATOR);
     }
 
     @Override
     public List<Student> sortStudentsByName(Collection<Student> students) {
-        return streamToList(sort(students.stream()));
+        return sort(students.stream(), NAME_COMPARATOR);
     }
 
     @Override
     public List<Student> findStudentsByFirstName(Collection<Student> students, String name) {
-        // :NOTE find(students, Student::getFirstName, name)
-        return streamToList(sort(findStream(students, s -> s.getFirstName().equals(name))));
+        return find(students, name, Student::getFirstName);
     }
 
     @Override
     public List<Student> findStudentsByLastName(Collection<Student> students, String name) {
-        return streamToList(sort(findStream(students, s -> s.getLastName().equals(name))));
+        return find(students, name, Student::getLastName);
     }
 
     @Override
     public List<Student> findStudentsByGroup(Collection<Student> students, GroupName group) {
-        return streamToList(sort(findByGroupStream(students, group)));
+        return find(students, group, Student::getGroup);
     }
 
     @Override
@@ -88,23 +80,15 @@ public class StudentDB implements StudentQuery {
                 ));
     }
 
-    private <T> Stream<T> getStream(List<Student> students, Function<Student, T> mapper) {
-        return students.stream().map(mapper);
+    private static <T> List<T> get(List<Student> students, Function<Student, T> func) {
+        return students.stream().map(func).collect(Collectors.toCollection(ArrayList::new));
     }
 
-    private <T> List<T> streamToList(Stream<T> stream) {
-        return stream.collect(Collectors.toCollection(ArrayList::new));
+    private static List<Student> sort(Stream<Student> students, Comparator<Student> comparator) {
+        return students.sorted(comparator)
+                .collect(Collectors.toCollection(ArrayList::new));
     }
 
-    private Stream<Student> sort(Stream<Student> students) {
-        return students.sorted(NAME_COMPARATOR);
+    private static <T> List<Student> find(Collection<Student> students, T param, Function<Student, T> f) {
+        return sort(students.stream().filter(s -> f.apply(s).equals(param)), NAME_COMPARATOR);
     }
-
-    private Stream<Student> findStream(Collection<Student> students, Predicate<Student> f) {
-        return students.stream().filter(f);
-    }
-
-    private Stream<Student> findByGroupStream(Collection<Student> students, GroupName group) {
-        return findStream(students, s -> s.getGroup().equals(group));
-    }
-}
